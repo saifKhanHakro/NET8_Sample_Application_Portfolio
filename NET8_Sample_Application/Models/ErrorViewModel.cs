6c5a7ea450c5dcfd62c4c9dc091b1477548750e1
saifullahhakro <saifullah.lutufullah@purevpn.com>
2023-12-06 16:39:55 +0500

    Add project files.

---
+++ NET8_Sample_Application/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace NET8_Sample_Application.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
